2c6a36c1060ac934b54263108f7f70566d0dd9e1
jvsoarez <joaovictorcpsa@gmail.com>
2023-02-13 17:37:50 -0300

    feat: renders a list of planets
    render array list with planet information, using planet card component
    
    Requisite 6 - done

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="planetas" />
+        {planets.map(({ name, image }) => (
+          <PlanetCard
+            data-testid="planet-card"
+            key={ name }
+            planetName={ name }
+            planetImage={ image }
+          />
+        ))}
       </div>
     );
   }

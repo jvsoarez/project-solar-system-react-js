5062083f6807e95044d9d8c1cd37008f6fbf3ab6
jvsoarez <joaovictorcpsa@gmail.com>
2023-02-13 23:59:36 -0300

    feat: renders 'Title' in 'Missions' component
    Requisite 8 - done

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -7,7 +7,7 @@ class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
-        <Title headline="planetas" />
+        <Title headline="Planetas" />
         {planets.map(({ name, image }) => (
           <PlanetCard
             data-testid="planet-card"
